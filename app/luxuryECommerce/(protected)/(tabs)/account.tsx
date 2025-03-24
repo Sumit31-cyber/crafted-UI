96f1c76e04aedda42284e616556fa7ae7a6e40cd
Sumit31-cyber <sumitkum9305@gmail.com>
2025-03-25 01:02:34 +0530

    Profile screen

--- app/luxuryECommerce/(protected)/(tabs)/account.tsx
+++ app/luxuryECommerce/(protected)/(tabs)/account.tsx
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import BlurBackdrop from "@/components/ui/BlurBackdrop";
+import useCustomHeader from "@/customHooks/LuxuryECommernceHooks/useCustomHeader";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 const Account = () => {
+  const { Header, headerHeight } = useCustomHeader();
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+      <BlurBackdrop />
+      <Header
+        icon={<SimpleLineIcons name="handbag" size={18} color="black" />}
+        onPress={() => {}}
+        title="Make the most of the app"
+      />
       <Text>Account</Text>
     </View>
   );
