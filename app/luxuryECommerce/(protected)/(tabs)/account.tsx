bdb7b9e3223183f23aeeccc3dec87d136b752699
Sumit31-cyber <sumitkum9305@gmail.com>
2025-03-26 01:35:35 +0530

    account UI

--- app/luxuryECommerce/(protected)/(tabs)/account.tsx
+++ app/luxuryECommerce/(protected)/(tabs)/account.tsx
@@ -1,8 +1,19 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import BlurBackdrop from "@/components/ui/BlurBackdrop";
 import useCustomHeader from "@/customHooks/LuxuryECommernceHooks/useCustomHeader";
-import { SimpleLineIcons } from "@expo/vector-icons";
+import {
+  AntDesign,
+  Entypo,
+  MaterialCommunityIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
 import {
   _horizontalPadding,
   _windowWidth,
@@ -13,8 +24,10 @@ import {
 import { Image } from "expo-image";
 import { BlurView } from "expo-blur";
 import CreditCard from "@/components/ui/LuxuryECommerce/CreditCard";
+import { BoxIcon } from "@/assets/svgs/luxuryECommSvgs/svgs";
 
 const _imageSize = _windowWidth * 0.3;
+const _borderRadius = 14;
 const Account = () => {
   const { Header, headerHeight } = useCustomHeader();
   return (
@@ -49,7 +62,7 @@ const Account = () => {
               shadowOpacity: 0.1,
               shadowRadius: 7.49,
 
-              elevation: 12,
+              elevation: 6,
             }}
           >
             <View
@@ -115,36 +128,44 @@ const Account = () => {
           </View>
 
           <CreditCard />
-
-          {/* <View
-            style={{
-              height: _imageSize,
-              aspectRatio: 1,
-              borderRadius: 100,
-              alignSelf: "center",
-              overflow: "hidden",
-            }}
-          >
-            <Image
-              source={{
-                uri: "https://images.unsplash.com/photo-1742558701206-55aa0c21cc8b?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          <View style={{}}>
+            <Text
+              style={{
+                fontSize: FontSizes.xLarge,
+                fontFamily: FONTS.poppinsRegular,
               }}
-              style={{ flex: 1 }}
-            />
+            >
+              My Orders
+            </Text>
+            <View style={{ gap: 15, marginVertical: 10 }}>
+              <Tiles
+                icon={<BoxIcon tint="#5fadf5" />}
+                title="All Orders"
+                onPress={() => {}}
+              />
+              <Tiles
+                icon={<Entypo name="cycle" size={30} color="#9a4dff" />}
+                title="Processing"
+                onPress={() => {}}
+              />
+              <Tiles
+                icon={
+                  <MaterialCommunityIcons
+                    name="truck-outline"
+                    size={30}
+                    color="#31a042"
+                  />
+                }
+                title="Shipped"
+                onPress={() => {}}
+              />
+              <Tiles
+                icon={<AntDesign name="back" size={30} color="#ea4b50" />}
+                title="Returns"
+                onPress={() => {}}
+              />
+            </View>
           </View>
-          <View
-            style={{
-              height: 400,
-              width: "100%",
-              top: -_imageSize / 2,
-              zIndex: -1,
-              paddingTop: _imageSize / 2,
-              borderTopLeftRadius: 20,
-              borderTopRightRadius: 20,
-            }}
-          >
-            <Text>Hello</Text>
-          </View> */}
         </View>
       </ScrollView>
     </View>
@@ -154,3 +175,61 @@ const Account = () => {
 export default Account;
 
 const styles = StyleSheet.create({});
+
+type TileProps = {
+  icon: React.ReactNode;
+  title: string;
+  onPress: () => void;
+};
+const Tiles = ({ icon, title, onPress }: TileProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.8}
+      style={{
+        flexDirection: "row",
+        backgroundColor: "white",
+        alignItems: "center",
+        paddingHorizontal: 12,
+        paddingVertical: 18,
+        borderRadius: _borderRadius,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 6,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 7.49,
+
+        elevation: 6,
+      }}
+    >
+      <View
+        style={{
+          height: 40,
+          width: 40,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {icon}
+      </View>
+      <Text
+        style={{
+          fontFamily: FONTS.poppinsMedium,
+          fontSize: FontSizes.medium,
+          marginLeft: 10,
+          color: LuxuryColors.liteBlack,
+        }}
+      >
+        {title}
+      </Text>
+      <AntDesign
+        style={{ marginLeft: "auto" }}
+        name="right"
+        size={20}
+        color={LuxuryColors.liteBlack}
+      />
+    </TouchableOpacity>
+  );
+};
